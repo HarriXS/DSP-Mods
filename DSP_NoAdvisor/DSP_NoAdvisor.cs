cdbe36457d56c6b18f5f964576fb20fadcf9f871
HarriE <72529503+HarriXS@users.noreply.github.com>
2021-02-03 20:15:53 +0000

    Add No Advisor Mod

---
+++ DSP_NoAdvisor/DSP_NoAdvisor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+
+namespace DSP_NoAdvisor
+{
+    [BepInPlugin("org.bepinex.plugins.noadvisor", "No Advisor Plug-in", "1.0.0.0")]
+    public class DSP_NoAdvisor : BaseUnityPlugin
+    {
+        internal void Awake()
+        {
+            var harmony = new Harmony("org.bepinex.plugins.noadvisor");
+            Harmony.CreateAndPatchAll(typeof(NoPlayAdvisorTip));
+            Harmony.CreateAndPatchAll(typeof(NoRunAdvisorTip));
+        }
+
+        [HarmonyPatch(typeof(UIAdvisorTip), "PlayAdvisorTip")]
+        public class NoPlayAdvisorTip
+        {
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                codes[0].opcode = OpCodes.Ret;
+                return codes.AsEnumerable();
+            }
+        }
+
+        [HarmonyPatch(typeof(UIAdvisorTip), "RunAdvisorTip")]
+        public class NoRunAdvisorTip
+        {
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                codes[0].opcode = OpCodes.Ret;
+                return codes.AsEnumerable();
+            }
+        }
+    }
+}
